1ef01b943f1eeb0c9b3a81cb4c02ef73a243ad86
OOXXXX <zhupx2003@126.com>
2022-08-09 20:41:02 +0800

    Add Mltiplication table file

---
+++ Mltiplication table/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Patrick on 2022/8/9.
+//
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i < 10; ++i) {
+        for (int j = 1; j < 10; ++j) {
+            if (i < j) break;
+            printf("%d x %d = %d ", i, j, i * j);
+        }
+        printf("\n");
+    }
+}
