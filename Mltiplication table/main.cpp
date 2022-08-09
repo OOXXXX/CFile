a77c06ad3d7a89ae746734fc894be5387fbf4891
OOXXXX <zhupx2003@126.com>
2022-08-10 01:23:55 +0800

    Fixed bug

--- Mltiplication table/main.cpp
+++ Mltiplication table/main.cpp
@@ -7,8 +7,7 @@ int main() {
     for (int i = 1; i < 10; ++i) {
         for (int j = 1; j < 10; ++j) {
             if (i < j) break;
-            printf("%d x %d = %d ", i, j, i * j);
-
+            printf("%d x %d = %2d ", i, j, i * j);
         }
         printf("\n");
     }
