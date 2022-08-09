bd4afd4b33fc8462582f8bb7ea478334f4fae9c4
OOXXXX <zhupx2003@126.com>
2022-08-10 03:43:19 +0800

    First Method to make bubble sort

---
+++ Bubble sorts/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Patrick on 2022/8/10.
+//
+#include <stdio.h>
+
+int main() {
+    int a[10] = {1, 3, 5, 4, 2, 6, 7, 8, 10, 9};
+    int i, j, temp;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10 - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    for (i = 0; i < 10; i++) {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
